eb0072f92e4b6608865c8b8d64ff46c9754ace50
sandakelum priyamantha <sandakelum.dev@gmail.com>
2022-09-30 12:01:24 +0530

    xlogs: changes for v0.0.1

--- xlogs.go
+++ xlogs.go
@@ -1,1 +1,28 @@
 package xlogs
+
+import "log"
+
+func Info(msg ...any) {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	log.SetPrefix("💬INFO : ")
+	log.Println(msg...)
+
+}
+
+func Warn(msg ...any) {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	log.SetPrefix("🚧WARN : ")
+	log.Println(msg...)
+}
+
+func Debug(msg ...any) {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	log.SetPrefix("🛑DEBUG : ")
+	log.Println(msg...)
+}
+
+func Error(msg ...any) {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	log.SetPrefix("❌ERROR : ")
+	log.Println(msg...)
+}
